9c36136b3794eb9042bee796c5c3588b6b4d1eac
Branden Ghena <brghena@umich.edu>
2016-09-21 16:07:23 -0400

    Bugfixes in i2c selector

--- software/apps/libs/i2c_selector.c
+++ software/apps/libs/i2c_selector.c
@@ -6,13 +6,13 @@
 
 
 int i2c_selector_set_callback(subscribe_cb callback, void* callback_args) {
-    return subscribe(DRIVER_NUM_GPIO_ASYNC, 0, callback, callback_args);
+    return subscribe(DRIVER_NUM_I2C_SELECTOR, 0, callback, callback_args);
 }
 
 int i2c_selector_select_channels(uint32_t channels) {
-	return command(DRIVER_NUM_GPIO_ASYNC, 0, channels);
+	return command(DRIVER_NUM_I2C_SELECTOR, 0, channels);
 }
 
-int i2c_selector_select_channels() {
-	return command(DRIVER_NUM_GPIO_ASYNC, 1, 0);
+int i2c_selector_disable_all_channels() {
+	return command(DRIVER_NUM_I2C_SELECTOR, 1, 0);
 }
