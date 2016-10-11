f36c59fa64c06ce1ac2d0f16901f91ca22894b26
Josh Adkins <adkinsjd@umich.edu>
2016-10-10 23:15:25 -0700

    added a very simply audio test for salae debugging

---
+++ software/apps/audio_test/main.c
@@ -0,0 +1,45 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <tock.h>
+#include "gpio.h"
+
+#define STROBE 4
+#define RESET 5 
+#define	POWER 6
+
+int main () {
+	gpio_enable_output(STROBE);
+	gpio_enable_output(RESET);
+	gpio_enable_output(POWER); 
+
+	gpio_clear(POWER);
+	gpio_clear(STROBE);
+	gpio_clear(RESET);
+
+	
+	delay_ms(1000);
+  	while (1) {
+		delay_ms(10);
+		gpio_set(STROBE);
+		gpio_set(RESET);
+		delay_ms(10);
+		gpio_clear(STROBE);
+		delay_ms(10);
+		gpio_clear(RESET);
+		gpio_set(STROBE);
+		delay_ms(10);
+		gpio_clear(STROBE);
+	
+		for(uint8_t i = 0; i < 7; i++) {
+			delay_ms(10);
+			gpio_set(STROBE);
+			delay_ms(10);
+			gpio_clear(STROBE);
+		}
+  	}
+}
