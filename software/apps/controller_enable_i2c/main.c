36dc5be939221929be1b218b2def6359e7335519
Brad Campbell <bradjc5@gmail.com>
2016-10-12 15:37:02 -0400

    update i2c slave test app for controller

--- software/apps/controller_enable_i2c/main.c
+++ software/apps/controller_enable_i2c/main.c
@@ -10,27 +10,7 @@
 #include "tock_str.h"
 #include "signpost_energy.h"
 
-// #define MOD0_GPIOA_PORT_NUM 0
-// #define MOD1_GPIOA_PORT_NUM 1
-// #define MOD2_GPIOA_PORT_NUM 2
-// #define MOD5_GPIOA_PORT_NUM 3
-// #define MOD6_GPIOA_PORT_NUM 4
-// #define MOD7_GPIOA_PORT_NUM 5
 
-// #define UNUSED_PARAMETER(x) (void)(x)
-
-// // Global store
-// int _data = 5;
-// int _data2 = 6;
-
-// // Callback when the pressure reading is ready
-// void callback (int callback_type, int data, int data2, void* callback_args) {
-//   UNUSED_PARAMETER(callback_args);
-
-//   _data = data;
-//   _data2 = data2;
-
-// }
 
 int _length;
 int _go = 0;
@@ -64,28 +44,51 @@ static void i2c_master_slave_callback (
 
 void print_data () {
   char buf[64];
-  sprintf(buf, "I2C Slave: %i (%02x %02x %02x %02x %02x %02x)\n", _length, slave_write_buf[0],
-                                                                 slave_write_buf[1],
-                                                                 slave_write_buf[2],
-                                                                 slave_write_buf[3],
-                                                                 slave_write_buf[4],
-                                                                 slave_write_buf[5]);
-  putstr(buf);
+
+  // Need at least two bytes to be a valid signpost message.
+  if (_length < 2) {
+    return;
+  }
+
+  // First byte is the sender
+  int sender_address = slave_write_buf[0];
+  // Second byte is the packet type
+  int message_type = slave_write_buf[1];
+
+  // Handle each type of message.
+  switch (sender_address) {
+    case 0x31: { // 802.15.4 scanner
+      if (message_type == 1 && _length == (16+2)) {
+        // Got valid message from 15.4 scanner
+        putstr("Message type 1 from Scanner15.4\n");
+        for (int channel=11; channel<27; channel++) {
+          sprintf(buf, "  Channel %i RSSI: %i\n", channel, (int) ((int8_t) slave_write_buf[2+(channel-11)]));
+          putstr(buf);
+        }
+      }
+
+      break;
+    }
+    default: {
+      sprintf(buf, "Different message? %i\n", sender_address);
+      putstr(buf);
+    }
+  }
+
 }
 
 int main () {
   putstr("[Controller] Start!\n");
-  // int energy;
 
-  // signpost_energy_init();
-
+  // Setup backplane
   gpio_async_set_callback(gpio_async_callback, NULL);
   controller_init_module_switches();
   controller_all_modules_enable_power();
   controller_all_modules_enable_i2c();
 
+  // Setup I2C listen
   i2c_master_slave_set_callback(i2c_master_slave_callback, NULL);
-  i2c_master_slave_set_slave_address(0x02);
+  i2c_master_slave_set_slave_address(0x20);
 
   i2c_master_slave_set_master_read_buffer(master_read_buf, 256);
   i2c_master_slave_set_master_write_buffer(master_write_buf, 256);
@@ -93,8 +96,6 @@ int main () {
   i2c_master_slave_set_slave_write_buffer(slave_write_buf, 256);
 
   i2c_master_slave_listen();
-
-
 
   while (1) {
     yield();
@@ -105,103 +106,4 @@ int main () {
       print_data();
     }
   }
-
-
-  // int i;
-
-  // while (1) {
-
-  //   putstr("\nChecking Energy\n");
-
-  //   for (i=0; i<8; i++) {
-  //     if (i == 3 || i == 4) continue;
-
-  //     energy = signpost_energy_get_module_energy(i);
-  //     print_data(i, energy);
-  //   }
-
-  //   energy = signpost_energy_get_controller_energy();
-  //   print_data(3, energy);
-
-  //   energy = signpost_energy_get_linux_energy();
-  //   print_data(4, energy);
-
-
-  //   delay_ms(500);
-  // }
-
-
-
-
-
-
-
-
-
-  // putstr("Welcome to Tock...lets wait for an interrupt!!\n");
-
-
-  // // Pass a callback function to the kernel
-  // smbus_interrupt_set_callback(callback, NULL);
-  // i2c_selector_set_callback(callback, NULL);
-  // ltc2941_set_callback(callback, NULL);
-
-  // // Read from first selector, chan
-  // i2c_selector_select_channels(0x01);
-  // yield();
-
-  // // // Reset charge
-  // // ltc2941_reset_charge();
-  // // yield();
-  // // And tell it any interrupt that may have existed has been handled
-  // smbus_interrupt_issue_alert_response();
-  // yield();
-
-  // // Read status, first byte should be 0x00
-  // int carltc2941_get_charge();
-  // yield();
-  // print_data(0);
-
-  // // // Set high threshold really low so we get a fast interrupt
-  // // ltc2941_set_high_threshold(0x0000);
-  // // yield();
-
-  // // // Open all channels so any gauge can interrupt and respond to SMBUS Alert Response
-  // // // Even though all gauges share same address, and multiple may be interrupting
-  // // // they will all respond with the same address
-  // // i2c_selector_select_channels(0xFF);
-
-  // // // Wait for interrupt, then print address of who interrupted
-  // // // Should be 0xc8 (gauge address with a 1 tacked on the end)
-  // // putstr("Waiting...\n");
-  // // yield();
-  // // print_data(0);
-
-  // // putstr("Reading Interrupts\n");
-
-  // // // Query the i2c_selector driver for who interrupted
-  // // i2c_selector_read_interrupts();
-  // // yield();
-  // // print_data(0);
-
-  // // i2c_selector_select_channels(_data2);
-  // // yield();
-
-  // // // Reset charge and handle interrupt
-  // // ltc2941_reset_charge();
-  // // yield();
-  // // smbus_interrupt_issue_alert_response();
-  // // yield();
-
-  // // // Read status twice
-  // // ltc2941_read_status();
-  // // yield();
-  // // print_data(0);
-  // // ltc2941_read_status();
-  // // yield();
-  // // print_data(0);
-
-  // while(1) {
-  //   yield();
-  // }
 }
