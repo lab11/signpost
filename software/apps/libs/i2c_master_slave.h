08d7bdf37293e093bde844c8847b63ceedbd3f9b
Josh Adkins <adkinsjd@umich.edu>
2016-10-12 01:47:09 -0700

    added some definitions for the different callbacks

--- software/apps/libs/i2c_master_slave.h
+++ software/apps/libs/i2c_master_slave.h
@@ -1,6 +1,13 @@
-#pragma once
+#ifndef I2C_MASTER_SLAVE
+#define I2C_MASTER_SLAVE
 
 #define DRIVER_NUM_I2CMASTERSLAVE 105
+
+#define CB_SLAVE_READ_REQUEST 	2
+#define CB_SLAVE_READ_COMPLETE 	4
+#define CB_SLAVE_WRITE 			3
+#define CB_MASTER_READ 			1
+#define CB_MASTER_WRITE 		0
 
 int i2c_master_slave_set_callback (subscribe_cb callback, void* callback_args);
 int i2c_master_slave_set_master_write_buffer(uint8_t* buffer, uint32_t len);
@@ -11,3 +18,5 @@ int i2c_master_slave_write(uint8_t address, uint8_t length);
 int i2c_master_slave_read(uint16_t address, uint16_t len);
 int i2c_master_slave_listen();
 int i2c_master_slave_set_slave_address(uint8_t address);
+
+#endif
