d2e678752bdd5eeb7bf8d7400f52e667c1e1bf05
Brad Campbell <bradjc5@gmail.com>
2016-10-12 02:45:37 -0400

    add i2c to scanner code

--- modules/spectrum154/contiki/apps/scanner154/main.c
+++ modules/spectrum154/contiki/apps/scanner154/main.c
@@ -6,6 +6,7 @@
 #include "dev/leds.h"
 
 #include "net/netstack.h"
+#include "dev/i2c.h"
 #include "cc2538-rf.h"
 
 
@@ -16,7 +17,7 @@ PROCESS(scanner154_process, "Scanner 15.4");
 AUTOSTART_PROCESSES(&scanner154_process);
 /*---------------------------------------------------------------------------*/
 
-
+int8_t out_buf[18] = {0};
 int channel_rssi[16] = {0};
 int current_channel = CC2538_RF_CHANNEL_MIN;
 int sample_index = 0;
@@ -26,6 +27,12 @@ int sample_index = 0;
 PROCESS_THREAD(scanner154_process, ev, data) {
 
 	PROCESS_BEGIN();
+
+	// Setup buffer for signpost network
+	out_buf[0] = 0x31; // my i2c address
+	out_buf[1] = 1; // message type
+
+	i2c_init(I2C_SDA_PORT, I2C_SDA_PIN, I2C_SCL_PORT, I2C_SCL_PIN, 400000);
 
 	// etimer_set(&periodic_timer_blue, CLOCK_SECOND/200);
 	etimer_set(&periodic_timer_blue, CLOCK_SECOND/100);
@@ -44,14 +51,23 @@ PROCESS_THREAD(scanner154_process, ev, data) {
 				// calculate average
 				channel_rssi[current_channel-CC2538_RF_CHANNEL_MIN] /= (20*20);
 
+				out_buf[(current_channel-CC2538_RF_CHANNEL_MIN)-2] = (int8_t) channel_rssi[current_channel-CC2538_RF_CHANNEL_MIN];
+
 				// Next!
 				sample_index = 0;
 
 				if (current_channel == CC2538_RF_CHANNEL_MAX) {
 					// print em
-					for (int i=0; i<16; i++) {
-						printf("Channel %i, RSSI %i\n", i+CC2538_RF_CHANNEL_MIN, channel_rssi[i]);
+					// for (int i=0; i<16; i++) {
+					// 	printf("Channel %i, RSSI %i\n", i+CC2538_RF_CHANNEL_MIN, channel_rssi[i]);
+					// }
+
+					// send to radio
+					uint8_t ret = i2c_burst_send(0x22, (uint8_t*) out_buf, 18);
+					if (ret != I2C_MASTER_ERR_NONE) {
+						i2c_master_command(I2C_MASTER_CMD_BURST_SEND_FINISH);
 					}
+
 
 					current_channel = CC2538_RF_CHANNEL_MIN;
 				} else {
