8d5f904a54d84f4a2cbbdd04a70abcd9b428f8c4
Brad Campbell <bradjc5@gmail.com>
2016-10-15 21:03:46 -0400

    add microwave_radar_fake

---
+++ software/apps/microwave_radar_fake/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#include "firestorm.h"
+#include "gpio.h"
+#include "i2c_master_slave.h"
+
+uint8_t slave_read_buf[256];
+uint8_t slave_write_buf[256];
+uint8_t master_read_buf[256];
+uint8_t master_write_buf[256];
+
+// Callback when the pressure reading is ready
+static void i2c_master_slave_callback (
+        int callback_type __attribute__ ((unused)),
+        int pin_value __attribute__ ((unused)),
+        int unused __attribute__ ((unused)),
+        void* callback_args __attribute__ ((unused))
+        ) {
+}
+
+int main (void) {
+    printf("[Microwave Radar Fake]\n");
+
+    // For LED
+    gpio_enable_output(9);
+
+    i2c_master_slave_set_callback(i2c_master_slave_callback, NULL);
+    i2c_master_slave_set_master_read_buffer(master_read_buf, 256);
+    i2c_master_slave_set_master_write_buffer(master_write_buf, 256);
+    i2c_master_slave_set_slave_read_buffer(slave_read_buf, 256);
+    i2c_master_slave_set_slave_write_buffer(slave_write_buf, 256);
+
+    i2c_master_slave_set_slave_address(0x32);
+
+    while (1) {
+        gpio_toggle(9);
+
+        master_write_buf[0] = 0x34;
+        master_write_buf[1] = 0x00;
+        master_write_buf[2] = 0x00;
+        master_write_buf[3] = 0x00;
+        master_write_buf[4] = 0x00;
+        master_write_buf[5] += 2;
+        master_write_buf[6] = 0x00;
+        master_write_buf[7] = 0x00;
+        master_write_buf[8] = 0x00;
+        master_write_buf[9] += 3;
+
+        i2c_master_slave_write(0x22, 10);
+        yield();
+
+        delay_ms(3000);
+    }
+}
