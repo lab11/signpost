cdb956e09c65413a336df0eaebc9f4e9ed247fc7
Josh Adkins <adkinsjd@umich.edu>
2016-10-17 01:04:57 -0700

    audio demo working
    I wanted to abstract away the msgeq7 but whenever I tried the
    analog signal got all nasty looking. I don't know why.
    This seems to work

--- software/apps/audio_demo/main.c
+++ software/apps/audio_demo/main.c
@@ -5,63 +5,109 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-//tock includes
 #include <tock.h>
-#include "firestorm.h"
 #include <tock_str.h>
-#include "msgeq7.h"
+#include "firestorm.h"
+#include "gpio.h"
+#include "adc.h"
 #include "i2c_master_slave.h"
 
+#define STROBE 4
+#define RESET 5
+#define	POWER 6
+
 #define BUFFER_SIZE 20
-//i2c buffers
+
 uint8_t slave_write_buf[BUFFER_SIZE];
 uint8_t slave_read_buf[BUFFER_SIZE];
 uint8_t master_read_buf[BUFFER_SIZE];
 uint8_t master_write_buf[BUFFER_SIZE];
 
-uint16_t result[7];
+static int adc_data;
+static int adc_done = 0;
 
-static void i2c_master_slave_callback (
-	int callback_type __attribute__ ((unused)),
-	int length __attribute__ ((unused)),
-	int unused __attribute__ ((unused)),
-	void * callback_args __attribute__ ((unused))) {
-	return;
+static void i2c_master_slave_callback(int callback_type, int length, int unused, void* callback_args) {
+    return;
 }
 
-static void timer_callback (
-	int callback_type __attribute__ ((unused)),
-	int length __attribute__ ((unused)),
-	int unused __attribute__ ((unused)),
-	void * callback_args __attribute__ ((unused))) {
-
-	msgeq7_get_values(result);
-	memcpy(master_write_buf+2, (uint8_t* )result, 14);
-	i2c_master_slave_write(0x22, 16);
+static void  adc_callback( int callback_type , int channel, int data, void* callback_args) {
+    adc_data = data;
+    adc_done = 1;
 }
+
+/*static void  timer_callback( int callback_type , int channel, int data, void* callback_args) {
+    i2c_master_slave_write(0x22,16);
+}*/
 
 int main () {
-	//low configure i2c slave to listen
-	i2c_master_slave_set_callback(i2c_master_slave_callback, NULL);
-	i2c_master_slave_set_slave_address(0x33);
-//
-	i2c_master_slave_set_master_read_buffer(master_read_buf, BUFFER_SIZE);
-	i2c_master_slave_set_master_write_buffer(master_write_buf, BUFFER_SIZE);
-	i2c_master_slave_set_slave_write_buffer(slave_write_buf, BUFFER_SIZE);
-	i2c_master_slave_set_slave_read_buffer(slave_read_buf, BUFFER_SIZE);
 
-	//listen
-	i2c_master_slave_listen();
+    //init i2c
+    i2c_master_slave_set_callback(i2c_master_slave_callback, NULL);
+    i2c_master_slave_set_slave_address(0x33);
 
-	timer_subscribe(timer_callback, NULL);
+    i2c_master_slave_set_master_read_buffer(master_read_buf, BUFFER_SIZE);
+    i2c_master_slave_set_master_write_buffer(master_write_buf, BUFFER_SIZE);
+    i2c_master_slave_set_slave_read_buffer(slave_read_buf, BUFFER_SIZE);
+    i2c_master_slave_set_slave_write_buffer(slave_write_buf, BUFFER_SIZE);
 
-	//msgeq7_initialize(4, 5, 0);
-	master_write_buf[0] = 0x33;
-	master_write_buf[1] = 0x00;
+    i2c_master_slave_listen();
 
-	timer_start_repeating(1000);
+    master_write_buf[0] = 0x33;
+    master_write_buf[1] = 0x01;
 
-	while(1) {
-		yield();
-	}
+    //init adc
+    adc_set_callback(adc_callback, NULL);
+    adc_initialize();
+	gpio_enable_output(STROBE);
+	gpio_enable_output(RESET);
+	gpio_enable_output(POWER);
+
+	gpio_clear(POWER);
+	gpio_clear(STROBE);
+	gpio_clear(RESET);
+
+    //timer_subscribe(timer_callback, NULL);
+    //timer_start_repeating(1000);
+    //i2c_master_slave_write(0x22,16);
+
+	delay_ms(1000);
+    uint8_t count;
+  	while (1) {
+		delay_ms(1);
+		gpio_set(STROBE);
+		gpio_set(RESET);
+		delay_ms(1);
+		gpio_clear(STROBE);
+		delay_ms(1);
+		gpio_clear(RESET);
+		gpio_set(STROBE);
+		delay_ms(1);
+		gpio_clear(STROBE);
+
+		for(uint8_t i = 0; i < 6; i++) {
+            adc_single_sample(0);
+            adc_done = 0;
+            while(adc_done == 0) {
+                yield();
+            }
+            master_write_buf[2+i*2] = (uint8_t)((adc_data >> 8) & 0xff);
+            master_write_buf[2+i*2+1] = (uint8_t)(adc_data & 0xff);
+			delay_ms(1);
+			gpio_set(STROBE);
+			delay_ms(1);
+			gpio_clear(STROBE);
+		}
+        adc_single_sample(0);
+        adc_done = 0;
+        while(adc_done == 0) {
+            yield();
+        }
+        master_write_buf[14] = (uint8_t)((adc_data >> 8) & 0xff);
+        master_write_buf[15] = (uint8_t)(adc_data & 0xff);
+        count++;
+        if(count >= 50) {
+            i2c_master_slave_write(0x22,16);
+            count = 0;
+        }
+  	}
 }
