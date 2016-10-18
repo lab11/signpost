c2c7fa154e3074d24eb8a7e2f422f8afab46df81
Branden Ghena <brghena@umich.edu>
2016-10-17 21:40:49 -0400

    Updated microwave radar application

--- software/apps/microwave_radar_test/main.c
+++ software/apps/microwave_radar_test/main.c
@@ -6,133 +6,171 @@
 #include <stdbool.h>
 
 #include <tock.h>
-#include <firestorm.h>
 #include <adc.h>
 #include <console.h>
 
 #define UNUSED_PARAMETER(x) (void)(x)
 
 // Global store
-uint32_t _channel = 0;
-uint32_t _adc_val = 0;
-uint32_t _timer = 0;
-uint32_t prev_data;
-uint32_t curr_data;
 uint32_t t1;
 uint32_t t2;
+uint32_t sample_index;
+uint32_t max_sample;
+uint32_t min_sample;
+bool active_mode = false;
+
+#define RISING 2
+#define FALLING 3
+#define FINISH_RISING 5
+#define FINISH_FALLING 6
+uint32_t sample_state;
 
 // Other global variable
-const uint32_t INITIAL_VAL = 0x820 << 4;
-const uint32_t LOWER_BOUND = 0X800 << 4;
-const uint32_t UPPER_BOUND = 0x840 << 4;
-const uint32_t NOISE_OFFSET = 4 << 4;
+const uint32_t INITIAL_VAL = 0x820;
+//const uint32_t LOWER_BOUND = 0X800;
+//const uint32_t UPPER_BOUND = 0x840;
+const uint32_t LOWER_BOUND = 0X750;
+const uint32_t UPPER_BOUND = 0x890;
+//const uint32_t NOISE_OFFSET = 4;
+const uint32_t NOISE_OFFSET = 10;
 
 // Some define
 #define SAMPLE_TIMES 20
 #define ADC_CHANNEL 0
-#define is_stable(curr, prev) (curr < prev + NOISE_OFFSET && curr > prev - NOISE_OFFSET)
 
 // used to store the interval of each half period
 uint32_t time_intervals[SAMPLE_TIMES];
 
+bool detect_motion (uint32_t sample) {
+    return (sample > UPPER_BOUND) || (sample < LOWER_BOUND);
+}
+
+uint32_t calculate_sample_frequency (uint32_t curr_data) {
+    // check if data signifies movement
+    if (!active_mode && detect_motion(curr_data)) {
+        // initialize active mode
+        active_mode = true;
+        sample_index = 0;
+
+        // starting this period sample
+        t1 = timer_read();
+        if (INITIAL_VAL < curr_data) {
+            sample_state = RISING;
+            max_sample = curr_data;
+            min_sample = curr_data;
+        } else {
+            sample_state = FALLING;
+            min_sample = curr_data;
+            max_sample = curr_data;
+        }
+    }
+
+    // sample frequencies
+    if (active_mode) {
+
+        // keep sampling until signals peaks or bottoms
+        if (sample_state == RISING) {
+            if (curr_data <= (max_sample - NOISE_OFFSET)) {
+                // data falling again
+                sample_state = FINISH_RISING;
+            } else {
+                if (curr_data > max_sample) {
+                    max_sample = curr_data;
+                }
+                sample_state = RISING;
+            }
+        } else if (sample_state == FALLING) {
+            if (curr_data >= (min_sample + NOISE_OFFSET)) {
+                // data rising again
+                sample_state = FINISH_FALLING;
+            } else {
+                if (curr_data < min_sample) {
+                    min_sample = curr_data;
+                }
+                sample_state = FALLING;
+            }
+        }
+
+        // calculate time for that period
+        if (sample_state == FINISH_RISING || sample_state == FINISH_FALLING) {
+            t2 = timer_read();
+            time_intervals[sample_index] = t2-t1;
+            sample_index++;
+
+            t1 = t2;
+            if (sample_state == FINISH_FALLING) {
+                sample_state = RISING;
+                max_sample = curr_data;
+                min_sample = curr_data;
+            } else if (sample_state == FINISH_RISING) {
+                sample_state = FALLING;
+                min_sample = curr_data;
+                max_sample = curr_data;
+            }
+
+            if (sample_index >= SAMPLE_TIMES) {
+                // average clock ticks between pos and neg edge
+                uint32_t average = 0;
+
+                // The first value is outlier. Don't account for that
+                for(int i = 1; i < SAMPLE_TIMES; i++) {
+                    average += time_intervals[i]/(SAMPLE_TIMES - 1);
+                }
+
+                // Since we are using timer with 32kHz frequency and prescaler 0
+                // One tick means 0.0000625 s
+                uint32_t interval = average * 625 * 2;
+                uint32_t frequency = 10000000/interval;
+                active_mode = false;
+
+                return frequency;
+            }
+        }
+    }
+
+    return 0;
+}
+
+uint32_t calculate_radar_speed (uint32_t freq) {
+    // Note: this speed is not really meaningful because it calculates
+    // the corresponding speed when object moving perpendicular to sensor
+    uint32_t speed_fph = (freq * 5280)/31;
+    uint32_t speed_fps = speed_fph*1000/3600;
+
+    printf("Freq: %d\tSpeed: %d (milli-fps)\n", freq, speed_fps);
+
+    return speed_fps;
+}
+
+
 // Callback when the adc reading is done
-void callback (int callback_type, int data, int data2, void* callback_args) {
-  UNUSED_PARAMETER(callback_args);
-  UNUSED_PARAMETER(callback_type);
+void adc_callback (int callback_type, int channel, int sample, void* callback_args) {
+    UNUSED_PARAMETER(callback_type);
+    UNUSED_PARAMETER(channel);
+    UNUSED_PARAMETER(callback_args);
 
-  _channel = data;
-  _adc_val = data2;
+    bool motion = detect_motion(sample);
+    uint32_t speed_fps = 0;
 
+    // determine mircowave radar frequency
+    uint32_t freq = calculate_sample_frequency(sample);
+    if (freq != 0) {
+        speed_fps = calculate_radar_speed(freq);
+    }
+
+    // get new sample
+    adc_single_sample(ADC_CHANNEL);
 }
 
-void print_timer () {
-  char buf[64];
-  sprintf(buf, "\tTimer: %d\n\n", _timer);
-  putstr(buf);
-}
-
-void print_data () {
-  char buf[64];
-  // the value read from adc register is left alligned to 16 bits
-  sprintf(buf, "\tGot: 0x%02x\n\n", _adc_val);
-  putstr(buf);
-}
-
-void sample_frequency(){
-  prev_data = INITIAL_VAL;
-  curr_data = _adc_val;
-
-  uint32_t i;
-
-  t2 = timer_read();
-
-
-  for(i = 0; i < SAMPLE_TIMES; ++i){
-  	t1 = t2;
-	if(prev_data < curr_data) {
-		while((prev_data < curr_data) || is_stable(curr_data, prev_data)){
-			adc_single_sample(ADC_CHANNEL);
-			prev_data = curr_data;
-			curr_data = _adc_val;
-			delay_ms(1);
-		}
-	} else if(prev_data >= curr_data) {
-		while((prev_data >= curr_data) || is_stable(curr_data, prev_data)){
-			adc_single_sample(ADC_CHANNEL);
-			prev_data = curr_data;
-			curr_data = _adc_val;
-			delay_ms(1);
-		}
-	}
-
-	t2 = timer_read();
-	time_intervals[i] = t2 - t1;
-
-  }
-
-  // average clock ticks between pos and neg edge
-  uint32_t average = 0;
-
-  // The first value is outlier. Don't account for that
-  for(i = 1; i < SAMPLE_TIMES; ++i) {
-  	average += time_intervals[i]/(SAMPLE_TIMES - 1);
-  }
-
- // Since we are using timer with 32kHz frequency and prescaler 0
- // One tick means 0.0000625 s
- uint32_t interval = average * 625 * 2;
- uint32_t frequency = 10000000/interval;
- // Note: this speed is not really meaningful because it calculates
- // the corresponding speed when object moving perpendicular to sensor
- uint32_t speed_fph = (frequency * 5280)/31;
-
-
-  char buf[64];
-  sprintf(buf, "\tFrequency is: %d, Speed is: %d feet per hour\n\n", frequency, speed_fph);
-  putstr(buf);
-
-  delay_ms(500);
-
-}
-
-bool detect_moving(){
-    return ((_adc_val) > UPPER_BOUND || (_adc_val) < LOWER_BOUND);
-}
 
 int main () {
-  putstr("Start Microwave Radar program. Sampling!!!\n");
+    putstr("Start Microwave Radar program. Sampling!!!\n");
 
-  adc_set_callback(callback, NULL);
-  // initialize adc
-  adc_initialize();
+    // initialize adc
+    adc_set_callback(adc_callback, NULL);
+    adc_initialize();
 
-
-  while (1) {
-    adc_single_sample(0);
-    yield();
-    //print_data();
-    if(detect_moving()){
-	sample_frequency();
-    }
-  }
+    // start getting samples
+    adc_single_sample(ADC_CHANNEL);
 }
+
