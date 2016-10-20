85b6631ad3bb8ec4b48920bdae5d6096529d017f
Brad Campbell <bradjc5@gmail.com>
2016-10-20 17:18:39 -0400

    BACKK TO TTYUSB1

--- software/receiver/lora_receive/signpost-lora-json.js
+++ software/receiver/lora_receive/signpost-lora-json.js
@@ -6,7 +6,8 @@ var mqtt = require('mqtt');
 // set the endpoint ID
 const DEVICE_ID = 0x09;
 const DEVICE_GROUP = 0x10;
-const SERIAL_PORT = '/dev/tty.usbserial-AJ0EB9M7';
+//const SERIAL_PORT = '/dev/tty.usbserial-AJ0EB9M7';
+const SERIAL_PORT = '/dev/ttyUSB1';
 const SPREADING_FACTOR = 7;
 
 var mqtt_client = mqtt.connect('mqtt://141.212.11.202');
