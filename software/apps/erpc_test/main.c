5e511b0c5b111dfb081ce437362fdd9307a40bb6
Josh Adkins <adkinsjd@umich.edu>
2016-11-05 01:33:16 -0700

    added empty erpc test code

---
+++ software/apps/erpc_test/main.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <tock.h>
+#include <firestorm.h>
+
+
+int main () {
+	delay_ms(1000);
+  	while (1) {
+
+  	}
+}
