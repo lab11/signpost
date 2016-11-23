e16b378750b6c13560129217b601c96fa1ba9574
Josh Adkins <adkinsjd@umich.edu>
2016-11-23 00:53:08 -0800

    added code for a simple erpc_test

--- software/apps/erpc_test/main.c
+++ software/apps/erpc_test/main.c
@@ -7,11 +7,29 @@
 
 #include <tock.h>
 #include <firestorm.h>
+#include <console.h>
+#include "test_arithmetic.h"
+#include "erpc_client_setup.h"
+#include "erpc_transport_setup.h"
 
 
 int main () {
-	delay_ms(1000);
-  	while (1) {
+    erpc_transport_t transport;
+    transport = erpc_transport_serial_init("out",115200);
+    erpc_client_init(transport);
 
+  	while (1) {
+        delay_ms(5000);
+        putstr("About to test the rpc\n");
+        delay_ms(5000);
+
+        float result = 0;
+        result =  add(5,6);
+
+        if(result == 11) {
+            //putstr("RPC Completed Correctly\n");
+        } else {
+            //putstr("RPC Failed\n");
+        }
   	}
 }
