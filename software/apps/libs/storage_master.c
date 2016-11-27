5a53353b2ee6e9a79d545fc6df03f94be7ae648e
Josh Adkins <adkinsjd@umich.edu>
2016-11-26 16:20:28 -0800

    edison wakeup function tested and working

--- software/apps/libs/storage_master.c
+++ software/apps/libs/storage_master.c
@@ -3,10 +3,12 @@
 
 void storage_master_enable_edison () {
     gpio_enable_output(EDISON_PWRBTN);
+    gpio_set(EDISON_PWRBTN);
 }
 
 void storage_master_wakeup_edison () {
     gpio_clear(EDISON_PWRBTN);
-    for(volatile uint8_t i = 0; i < 1000; i++);
+    //this is about 100ms. 10ms didn't work. Did try other values
+    for(volatile uint32_t i = 0; i < 100000; i++);
     gpio_set(EDISON_PWRBTN);
 }
