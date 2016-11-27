78d437ba61ea94e602f294fd8e12a5c8f1655fbd
Josh Adkins <adkinsjd@umich.edu>
2016-11-26 22:44:36 -0800

    updated this app for the new i2c stuff.
    seems to be mostly workign for read and writes

--- software/apps/storage_master_erpc/main.c
+++ software/apps/storage_master_erpc/main.c
@@ -21,10 +21,12 @@ uint8_t slave_read_buf[BUFFER_SIZE];
 
 uint8_t request_buf[BUFFER_SIZE];
 uint8_t return_buf[BUFFER_SIZE];
+uint8_t return_address;
 
 uint8_t reg = 0x00;
 #define RPC_REQUEST 0x01
 #define RPC_RETURN 0x02
+#define RPC_RETURN_ADDRESS 0x03
 
 static void i2c_master_slave_callback (
     int callback_type,
@@ -32,37 +34,46 @@ static void i2c_master_slave_callback (
     int unused __attribute__ ((unused)),
     void * callback_args __attribute__ ((unused))) {
 
-    //for now only take writes
     if(callback_type == CB_SLAVE_READ_REQUEST) {
-        //this is where you fulfill the rpc read from the edison
-        if(reg == RPC_REQUEST) {
-            //This allows the edison to read the most recent request
-            memcpy(slave_read_buf,request_buf,BUFFER_SIZE);
-        } else if(slave_write_buf[1] = RPC_RETURN) {
-            //this allows the microcontroller to read the edison's response
-            memcpy(slave_read_buf,return_buf,BUFFER_SIZE);
-        }
+        //this isn't really working right now
         return;
     } else if (callback_type == CB_SLAVE_READ_COMPLETE) {
         return;
     } else if (callback_type == CB_SLAVE_WRITE) {
         reg = slave_write_buf[0];
         if(length > 1) {
+            //assume this is a real request
             if(reg == RPC_REQUEST) {
                 //this is an rpc request
                 //
                 //copy the rpc request to the request buffer
-                memcpy(request_buf,slave_write_buf,length-1);
+                memcpy(request_buf,slave_write_buf+1,length-1);
+                memcpy(slave_read_buf,request_buf,BUFFER_SIZE);
 
                 //wakeup the edison to tell it that it has an rpc request
                 storage_master_wakeup_edison();
 
-            } else if(slave_write_buf[1] = RPC_RETURN) {
+            } else if(reg == RPC_RETURN) {
                 //this is an rpc return from the edison
                 //copy the rpc return to the return buffer
-                memcpy(return_buf,slave_write_buf,length-1);
+                memcpy(return_buf,slave_write_buf+1,length-1);
+                memcpy(slave_read_buf,return_buf,BUFFER_SIZE);
 
                 //alert the module that the request has returned
+                //should this happen over i2c?? I would like to just set the
+                //alert pin but that's on another microcontroller...
+            } else if(reg == RPC_RETURN_ADDRESS) {
+                memcpy(&return_address,slave_write_buf+1,1);
+                //memcpy(slave_read_buf,&return_address,BUFFER_SIZE);
+            }
+        } else {
+            //just setting the register - asume it's to read it soon
+            if(reg == RPC_REQUEST) {
+                memcpy(slave_read_buf,request_buf,BUFFER_SIZE);
+            } else if(reg == RPC_RETURN) {
+                memcpy(slave_read_buf,return_buf,BUFFER_SIZE);
+            } else if(reg == RPC_RETURN_ADDRESS) {
+                //no one should ever read this
             }
         }
     }
@@ -71,13 +82,14 @@ static void i2c_master_slave_callback (
 int main () {
     storage_master_enable_edison();
 
+    i2c_master_slave_set_slave_write_buffer(slave_write_buf, BUFFER_SIZE);
+    i2c_master_slave_set_slave_read_buffer(slave_read_buf, BUFFER_SIZE);
+
     //low configure i2c slave to listen
     i2c_master_slave_set_callback(i2c_master_slave_callback, NULL);
     i2c_master_slave_set_slave_address(0x19);
 
-    i2c_master_slave_set_slave_write_buffer(slave_write_buf, BUFFER_SIZE);
-    i2c_master_slave_set_slave_read_buffer(slave_read_buf, BUFFER_SIZE);
-
     //listen
     i2c_master_slave_listen();
+    i2c_master_slave_read_ready();
 }
