6a32c014aafdb549abd0b45db6d7068523e7bd23
Josh Adkins <adkinsjd@umich.edu>
2016-11-26 22:38:24 -0800

    cherry picked and updated version of i2c from master

--- software/kernel/signpost_drivers/src/i2c_master_slave_driver.rs
+++ software/kernel/signpost_drivers/src/i2c_master_slave_driver.rs
@@ -1,9 +1,21 @@
+//! Capsule for presenting both an I2C Master and I2C Slave interface to
+//! applications. By calling `listen` this module will wait for I2C messages
+//! send to it by other masters on the I2C bus. If this device wants to
+//! transmit as an I2C master, this module will put the I2C hardware in master
+//! mode, transmit the read/write, then go back to listening (if listening
+//! was enabled).
+//!
+//! This capsule must sit directly above the I2C HIL interface (and not
+//! on top of the mux) because there is no way to mux the slave (it can't
+//! listen on more than one address) and because the application may want
+//! to be able to talk to any I2C address.
+
 use core::cell::Cell;
 use core::cmp;
+use kernel::{AppId, AppSlice, Callback, Driver, Shared};
 
 use kernel::common::take_cell::TakeCell;
 use kernel::hil;
-use kernel::{AppId, AppSlice, Callback, Driver, Shared};
 
 pub static mut BUFFER1: [u8; 256] = [0; 256];
 pub static mut BUFFER2: [u8; 256] = [0; 256];
@@ -31,16 +43,16 @@ pub struct I2CMasterSlaveDriver<'a> {
     master_action: Cell<MasterAction>, // Whether we issued a write or read as master
     master_buffer: TakeCell<&'static mut [u8]>,
     slave_buffer1: TakeCell<&'static mut [u8]>,
-    _slave_buffer2: TakeCell<&'static mut [u8]>,
+    slave_buffer2: TakeCell<&'static mut [u8]>,
     app_state: TakeCell<AppState>,
 }
 
 impl<'a> I2CMasterSlaveDriver<'a> {
     pub fn new(i2c: &'a hil::i2c::I2CMasterSlave,
-                     master_buffer: &'static mut [u8],
-                     slave_buffer1: &'static mut [u8],
-                     slave_buffer2: &'static mut [u8],
-                     ) -> I2CMasterSlaveDriver<'a> {
+               master_buffer: &'static mut [u8],
+               slave_buffer1: &'static mut [u8],
+               slave_buffer2: &'static mut [u8])
+               -> I2CMasterSlaveDriver<'a> {
         let app_state = AppState {
             callback: TakeCell::empty(),
             master_tx_buffer: TakeCell::empty(),
@@ -55,14 +67,14 @@ impl<'a> I2CMasterSlaveDriver<'a> {
             master_action: Cell::new(MasterAction::Write),
             master_buffer: TakeCell::new(master_buffer),
             slave_buffer1: TakeCell::new(slave_buffer1),
-            _slave_buffer2: TakeCell::new(slave_buffer2),
+            slave_buffer2: TakeCell::new(slave_buffer2),
             app_state: TakeCell::new(app_state),
         }
     }
 }
 
 impl<'a> hil::i2c::I2CHwMasterClient for I2CMasterSlaveDriver<'a> {
-    fn command_complete(&self, buffer: &'static mut [u8], error:  hil::i2c::Error) {
+    fn command_complete(&self, buffer: &'static mut [u8], error: hil::i2c::Error) {
 
         // Map I2C error to a number we can pass back to the application
         let err: isize = match error {
@@ -115,62 +127,50 @@ impl<'a> hil::i2c::I2CHwMasterClient for I2CMasterSlaveDriver<'a> {
 }
 
 impl<'a> hil::i2c::I2CHwSlaveClient for I2CMasterSlaveDriver<'a> {
-    fn command_complete(&self, buffer: &'static mut [u8], length: u8, transmission_type:  hil::i2c::SlaveTransmissionType) {
+    fn command_complete(&self,
+                        buffer: &'static mut [u8],
+                        length: u8,
+                        transmission_type: hil::i2c::SlaveTransmissionType) {
 
-        // need to know if read or write
-
-        // on write, copy bytes to app slice and do callback
-        //           then pass buffer back to hw driver
-
-        // on read, just signal upper layer and replace the read buffer in this
-        //    driver
-
+        // Need to know if read or write
+        //   - on write, copy bytes to app slice and do callback
+        //     then pass buffer back to hw driver
+        //   - on read, just signal upper layer and replace the read buffer
+        //     in this driver
 
         match transmission_type {
             hil::i2c::SlaveTransmissionType::Write => {
                 self.app_state.map(|app_state| {
                     app_state.slave_rx_buffer.map(move |app_rx| {
-                        // self.master_buffer.take().map(|kernel_tx| {
-                            // Check bounds for write length
-                            let buf_len = cmp::min(app_rx.len(), buffer.len());
-                            let read_len = cmp::min(buf_len, length as usize);
+                        // Check bounds for write length
+                        let buf_len = cmp::min(app_rx.len(), buffer.len());
+                        let read_len = cmp::min(buf_len, length as usize);
 
-                            let d = &mut app_rx.as_mut()[0..read_len];
-                            for (i, c) in buffer[0..read_len].iter_mut().enumerate() {
-                                d[i] = *c;
-                            }
+                        let d = &mut app_rx.as_mut()[0..read_len];
+                        for (i, c) in buffer[0..read_len].iter_mut().enumerate() {
+                            d[i] = *c;
+                        }
 
-                            self.slave_buffer1.replace(buffer);
-
-                            // self.master_action.set(MasterAction::Read(read_len as u8));
-
-                            // hil::i2c::I2CMaster::enable(self.i2c);
-                            // hil::i2c::I2CMaster::read(self.i2c, address, kernel_tx, read_len as u8);
-
-
-                            // self.app_state.map(|app_state| {
-
-                            // });
-
-
-                        // });
+                        self.slave_buffer1.replace(buffer);
                     });
 
                     app_state.callback.map(|mut cb| {
                         cb.schedule(3, length as usize, 0);
                     });
                 });
-
-
             }
 
-            hil::i2c::SlaveTransmissionType::Read => { }
+            hil::i2c::SlaveTransmissionType::Read => {
+                self.slave_buffer2.replace(buffer);
+
+                // Notify the app that the read finished
+                self.app_state.map(|app_state| {
+                    app_state.callback.map(|mut cb| {
+                        cb.schedule(4, length as usize, 0);
+                    });
+                });
+            }
         }
-
-
-
-
-
     }
 
     fn read_expected(&self) {
@@ -178,14 +178,15 @@ impl<'a> hil::i2c::I2CHwSlaveClient for I2CMasterSlaveDriver<'a> {
         // has setup a buffer to read from.
         self.app_state.map(|app_state| {
             app_state.callback.map(|mut cb| {
-                // Ask the app to setup a read buffer.
+                // Ask the app to setup a read buffer. The app must call
+                // command 3 after it has setup the shared read buffer with
+                // the correct bytes.
                 cb.schedule(2, 0, 0);
             });
         });
     }
-
+    
     fn write_expected(&self) {
-        // panic!("what do i pass this buffer?");
         // Don't expect this to occur. We will typically have a buffer waiting
         // to receive bytes because this module has a buffer and may as well
         // just let the hardware layer have it. But, if it does happen
@@ -254,8 +255,6 @@ impl<'a> Driver for I2CMasterSlaveDriver<'a> {
                 let address = (data & 0xFFFF) as u8;
                 let len = (data >> 16) & 0xFFFF;
 
-
-
                 self.app_state.map(|app_state| {
                     app_state.master_tx_buffer.map(|app_tx| {
                         self.master_buffer.take().map(|kernel_tx| {
@@ -271,11 +270,13 @@ impl<'a> Driver for I2CMasterSlaveDriver<'a> {
                             self.master_action.set(MasterAction::Write);
 
                             hil::i2c::I2CMaster::enable(self.i2c);
-                            hil::i2c::I2CMaster::write(self.i2c, address, kernel_tx, write_len as u8);
+                            hil::i2c::I2CMaster::write(self.i2c,
+                                                       address,
+                                                       kernel_tx,
+                                                       write_len as u8);
                         });
                     });
                 });
-
 
                 0
             }
@@ -330,13 +331,30 @@ impl<'a> Driver for I2CMasterSlaveDriver<'a> {
             // Prepare for a read from another Master by passing what's
             // in the shared slice to the lower level I2C hardware driver.
             3 => {
+                self.app_state.map(|app_state| {
+                    app_state.slave_tx_buffer.map(|app_tx| {
+                        self.slave_buffer2.take().map(|kernel_tx| {
+                            // Check bounds for write length
+                            let len = data;
+                            let buf_len = cmp::min(app_tx.len(), kernel_tx.len());
+                            let read_len = cmp::min(buf_len, len);
+
+                            let d = &mut app_tx.as_mut()[0..read_len];
+                            for (i, c) in kernel_tx[0..read_len].iter_mut().enumerate() {
+                                *c = d[i];
+                            }
+
+                            hil::i2c::I2CSlave::read_send(self.i2c, kernel_tx, read_len as u8);
+                        });
+                    });
+                });
 
                 0
             }
 
             // Stop listening for messages as an I2C slave
             4 => {
-
+                hil::i2c::I2CSlave::disable(self.i2c);
 
                 // We are no longer listening for I2C messages from a different
                 // master device.
@@ -356,5 +374,3 @@ impl<'a> Driver for I2CMasterSlaveDriver<'a> {
         }
     }
 }
-
-
