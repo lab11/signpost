defdb4218558cd21d1ab6b18d32336b3c6477a1b
Josh Adkins <adkinsjd@umich.edu>
2016-11-27 16:44:49 -0800

    updated app to include most recent tests

--- software/apps/storage_master_erpc/main.c
+++ software/apps/storage_master_erpc/main.c
@@ -28,6 +28,16 @@ uint8_t reg = 0x00;
 #define RPC_RETURN 0x02
 #define RPC_RETURN_ADDRESS 0x03
 
+//Test procedure.
+//1) turn on signpost
+//2) another module writes data to 0x19
+//3) I see this occur on my salae
+//4) I also observe that the callback is getting called because the edison
+//wakeup pin works as expected
+//5) I read reg 0x01 from address 0x19
+//6) If the commented out lines in main are there it returns that data
+//      If the lines are commented out it returns all 0s
+
 static void i2c_master_slave_callback (
     int callback_type,
     int length,
@@ -49,7 +59,10 @@ static void i2c_master_slave_callback (
                 //copy the rpc request to the request buffer
                 memcpy(request_buf,slave_write_buf+1,length-1);
                 memcpy(slave_read_buf,request_buf,BUFFER_SIZE);
+                i2c_master_slave_read_ready(BUFFER_SIZE);
 
+                //I know that this write is occuring. I am observing
+                //the edison wakeup pin
                 //wakeup the edison to tell it that it has an rpc request
                 storage_master_wakeup_edison();
 
@@ -58,6 +71,7 @@ static void i2c_master_slave_callback (
                 //copy the rpc return to the return buffer
                 memcpy(return_buf,slave_write_buf+1,length-1);
                 memcpy(slave_read_buf,return_buf,BUFFER_SIZE);
+                i2c_master_slave_read_ready(BUFFER_SIZE);
 
                 //alert the module that the request has returned
                 //should this happen over i2c?? I would like to just set the
@@ -70,8 +84,10 @@ static void i2c_master_slave_callback (
             //just setting the register - asume it's to read it soon
             if(reg == RPC_REQUEST) {
                 memcpy(slave_read_buf,request_buf,BUFFER_SIZE);
+                i2c_master_slave_read_ready(BUFFER_SIZE);
             } else if(reg == RPC_RETURN) {
                 memcpy(slave_read_buf,return_buf,BUFFER_SIZE);
+                i2c_master_slave_read_ready(BUFFER_SIZE);
             } else if(reg == RPC_RETURN_ADDRESS) {
                 //no one should ever read this
             }
@@ -82,6 +98,11 @@ static void i2c_master_slave_callback (
 int main () {
     storage_master_enable_edison();
 
+    //If these lines are here the read works and returns this data
+    /*slave_read_buf[0] = 0x10;
+    slave_read_buf[1] = 0x11;
+    slave_read_buf[2] = 0x12;*/
+
     i2c_master_slave_set_slave_write_buffer(slave_write_buf, BUFFER_SIZE);
     i2c_master_slave_set_slave_read_buffer(slave_read_buf, BUFFER_SIZE);
 
@@ -91,5 +112,5 @@ int main () {
 
     //listen
     i2c_master_slave_listen();
-    i2c_master_slave_read_ready();
+    i2c_master_slave_read_ready(BUFFER_SIZE);
 }
