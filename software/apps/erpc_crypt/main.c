74c7bf08e245ab41d391e48634a9cda9ec7761fa
Josh Adkins <adkinsjd@umich.edu>
2016-12-02 13:12:37 -0800

    added a crypto erpc app
    This is a good example for lists and struct in the idle file

---
+++ software/apps/erpc_crypt/main.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <tock.h>
+#include <firestorm.h>
+#include <console.h>
+#include "test_crypt.h"
+#include "erpc_client_setup.h"
+#include "erpc_transport_setup.h"
+
+
+
+int main () {
+    erpc_transport_t transport;
+    transport = erpc_transport_i2c_master_slave_init(0x19,0x30);
+    erpc_client_init(transport);
+
+    ret* signed_data;
+    uint8_t data[100];
+
+    signed_data = sign(data,100);
+
+}
