0bac9fe3d5b5842cf6be7d07105beb5391cddba2
Josh Adkins <adkinsjd@umich.edu>
2016-12-04 01:17:21 -0800

    message library c++ compatible extern

--- software/apps/libs/message.h
+++ software/apps/libs/message.h
@@ -8,7 +8,9 @@
 //Here's the proposed packet structure
 //1:ver,2:total len, 3bit flag, remaind fragment offset, src
 
-
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 //initialize
 void message_init(uint8_t src);
@@ -22,5 +24,9 @@ uint32_t message_recv(uint8_t* data, uint32_t len, uint8_t* src);
 
 //set the read buffer
 uint32_t message_set_read_buffer(uint8_t* data, uint32_t len);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
