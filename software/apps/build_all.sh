606ccef3b0fb6a6e2f1ee2d2448eb913d4f7c55e
Pat Pannuto <pat.pannuto@gmail.com>
2017-02-02 22:11:12 -0500

    update clean/build_all for tests
    Also change build_all to collect all failures instead of failing out
    immediately

--- software/apps/build_all.sh
+++ software/apps/build_all.sh
@@ -8,12 +8,34 @@ red=$(tput setaf 1)
 green=$(tput setaf 2)
 blue=$(tput setaf 4)
 
-set -e
+# Don't fail on build failure, for now:
+#set -e
+
+# Instead collect and print a list
+declare -a failures
+
 for dir in `find . -maxdepth 1 -type d`; do
 	if [ $dir == "." ]; then continue; fi
 	if [ $dir == "./libs" ]; then continue; fi
+	if [ $dir == "./tests" ]; then continue; fi
 	echo "${bold}${blue}Compiling${black} $dir${normal}"
 	pushd $dir > /dev/null
-	make
+	make || failures+=($dir)
 	popd > /dev/null
 done
+
+for dir in `find tests -maxdepth 1 -type d`; do
+	if [ $dir == "tests" ]; then continue; fi
+	pushd $dir > /dev/null
+	make || failures+=($dir)
+	popd > /dev/null
+done
+
+if [ ${#failures[@]} -gt 0 ]; then
+	echo ""
+	echo "${bold}${red}Build Failures:${normal}"
+	for fail in ${failures[@]}; do
+		echo $fail
+	done
+	exit 1
+fi
--- software/apps/clean_all.sh
+++ software/apps/clean_all.sh
@@ -4,6 +4,14 @@ set -e
 for dir in `find . -maxdepth 1 -type d`; do
 	if [ $dir == "." ]; then continue; fi
 	if [ $dir == "./libs" ]; then continue; fi
+	if [ $dir == "./tests" ]; then continue; fi
+	pushd $dir
+	make clean
+	popd
+done
+
+for dir in `find tests -maxdepth 1 -type d`; do
+	if [ $dir == "tests" ]; then continue; fi
 	pushd $dir
 	make clean
 	popd
