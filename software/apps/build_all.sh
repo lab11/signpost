3850b6b07b5caa77e8202cd38113a0ef734b68db
Pat Pannuto <pat.pannuto@gmail.com>
2017-02-03 23:10:13 -0500

    add temporary support for skipping some builds

--- software/apps/build_all.sh
+++ software/apps/build_all.sh
@@ -6,7 +6,12 @@ normal=$(tput sgr0)
 black=$(tput setaf 0)
 red=$(tput setaf 1)
 green=$(tput setaf 2)
+yellow=$(tput setaf 3)
 blue=$(tput setaf 4)
+pink=$(tput setaf 5)
+teal=$(tput setaf 6)
+white=$(tput setaf 7)
+grey=$(tput setaf 8)
 
 # Don't fail on build failure, for now:
 #set -e
@@ -14,11 +19,21 @@ blue=$(tput setaf 4)
 # Instead collect and print a list
 declare -a failures
 
+declare -a skips
+
 for dir in `find . -maxdepth 1 -type d`; do
 	if [ $dir == "." ]; then continue; fi
 	if [ $dir == "./libs" ]; then continue; fi
 	if [ $dir == "./support" ]; then continue; fi
 	if [ $dir == "./tests" ]; then continue; fi
+
+	# XXX Temporary
+	if [ $dir == "./erpc_crypt" ]; then skips+=($dir-JOSH); continue; fi
+	if [ $dir == "./erpc_test" ]; then skips+=($dir-JOSH); continue; fi
+	if [ $dir == "./mbedtls_aes" ]; then skips+=($dir-PAT-stacksize); continue; fi
+	if [ $dir == "./mbedtls_hash" ]; then skips+=($dir-PAT-stacksize); continue; fi
+	if [ $dir == "./mbedtls_hmac" ]; then skips+=($dir-PAT-stacksize); continue; fi
+
 	echo "${bold}${blue}Compiling${black} $dir${normal}"
 	pushd $dir > /dev/null
 	make || failures+=($dir)
@@ -31,6 +46,14 @@ for dir in `find tests -maxdepth 1 -type d`; do
 	make || failures+=($dir)
 	popd > /dev/null
 done
+
+if [ ${#skips[@]} -gt 0 ]; then
+	echo ""
+	echo "${bold}${pink}Skipped:${normal}"
+	for skip in ${skips[@]}; do
+		echo $skip
+	done
+fi
 
 if [ ${#failures[@]} -gt 0 ]; then
 	echo ""
