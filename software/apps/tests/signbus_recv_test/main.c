f7040e461673a0b3509a95daf45f32acdbd9dafb
Pat Pannuto <pat.pannuto@gmail.com>
2017-02-15 00:51:01 -0500

    split app_send_recv_test into two tests

--- software/apps/tests/signbus_recv_test/main.c
+++ software/apps/tests/signbus_recv_test/main.c
@@ -12,11 +12,8 @@
 #include "rng.h"
 #include "signbus_app_layer.h"
 #include "signbus_io_interface.h"
-#include "signbus_protocol_layer.h"
 
 #define INTERVAL_IN_MS 2000
-
-//#define SENDER
 
 static uint8_t key[32];
 
@@ -28,7 +25,7 @@ static uint8_t message[1024];
 
 static bool recent_message = false;
 
-static void cb(size_t length) {
+static void cb(size_t length __attribute__((unused)) ) {
     // Print data for this received message
     printf("frame_type: %02x, api_type: %02x, message_type: %02x, message_len: %d, %02x\n",
             frame_type, api_type, message_type, message_length, message_length);
@@ -54,30 +51,21 @@ static void cb(size_t length) {
 
 int main(void) {
     memset(key, 0, 32);
-    strcpy(key, "this is a key");
+    strcpy((char *) key, "this is a key");
     printf("\n###\n\n\ntest app stack\n");
 
-#ifndef SENDER
     printf("RECEIVER: Begin listening\n\n");
 
     signbus_io_init(0x18);
     signbus_app_recv_async(cb, key,
             &frame_type, &api_type, &message_type,
             &message_length, message);
-#endif
     while(1) {
         delay_ms(INTERVAL_IN_MS);
-#ifdef SENDER
-        memcpy(message, "hello there\0", strlen("hello there") + 1);
-        signbus_io_init(0x32);
-        signbus_app_send(0x18, key, NotificationFrame, 0x00, 0x00, strlen("hello there") + 1, message);
-        printf("SENDER: sent message\n");
-#else
         if (recent_message == false) {
             printf("RECEIVER: No message in %d ms\n", INTERVAL_IN_MS);
         } else {
             recent_message = false;
         }
-#endif
     }
 }
---
+++ software/apps/tests/signbus_send_test/main.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <inttypes.h>
+
+#include "tock.h"
+#include "console.h"
+#include "timer.h"
+#include "rng.h"
+#include "signbus_app_layer.h"
+#include "signbus_io_interface.h"
+
+#define INTERVAL_IN_MS 2000
+
+static uint8_t key[32];
+static uint8_t message[1024];
+
+int main(void) {
+    memset(key, 0, 32);
+    strcpy((char *) key, "this is a key");
+    printf("\n###\n\n\ntest app stack\n");
+
+    while(1) {
+        delay_ms(INTERVAL_IN_MS);
+        memcpy(message, "hello there\0", strlen("hello there") + 1);
+        signbus_io_init(0x32);
+        signbus_app_send(0x18, key, NotificationFrame, 0x00, 0x00, strlen("hello there") + 1, message);
+        printf("SENDER: sent message\n");
+    }
+}
