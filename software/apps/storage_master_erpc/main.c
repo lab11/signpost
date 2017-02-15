9871852a6cf0aecb25e9d6dd4a407549764e06f8
Pat Pannuto <pat.pannuto@gmail.com>
2017-02-15 02:27:13 -0500

    bring storage_master app up to new names

--- software/apps/storage_master_erpc/main.c
+++ software/apps/storage_master_erpc/main.c
@@ -6,11 +6,11 @@
 #include <stdbool.h>
 
 //tock includes
-#include "tock.h"
 #include "console.h"
-#include "message.h"
 #include "gpio.h"
+#include "signbus_io_interface.h"
 #include "storage_master.h"
+#include "tock.h"
 
 #define BUFFER_SIZE 2048
 
@@ -25,7 +25,7 @@ static uint8_t rpc_pending = 0;
 #define RPC_RETURN 0x02
 
 int main (void) {
-    message_init(0x19);
+    signbus_io_init(0x19);
     storage_master_enable_edison();
     gpio_enable_output(2);
     gpio_set(2);
@@ -33,7 +33,7 @@ int main (void) {
     while(1) {
         uint8_t src;
         uint32_t len;
-        len = message_recv(message_buf, BUFFER_SIZE, &src);
+        len = signbus_io_recv(message_buf, BUFFER_SIZE, &src);
 
         if(message_buf[0] == RPC_REQUEST) {
             //use len > 1 to make sure that someone isn't just setting
@@ -42,7 +42,7 @@ int main (void) {
                 gpio_clear(2);
                 rpc_pending = 1;
                 memcpy(request_buf, message_buf+1,len-1);
-                message_set_read_buffer(request_buf,len-1);
+                signbus_io_set_read_buffer(request_buf,len-1);
                 return_address = src;
                 storage_master_wakeup_edison();
             }
@@ -51,7 +51,7 @@ int main (void) {
             gpio_set(2);
             memcpy(return_buf, message_buf+1,len-1);
             rpc_pending = 0;
-            message_send(return_address, return_buf, len-1);
+            signbus_io_send(return_address, return_buf, len-1);
         }
     }
 }
