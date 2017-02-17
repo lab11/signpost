dad13f53e786dcec7627f2d47b90bc5b24dafb02
Branden Ghena <brghena@umich.edu>
2017-02-17 01:12:36 -0500

    Start of a storage master API
    Capable of reading and writing records to the SD card reliably

--- software/apps/libs/storage_master.h
+++ software/apps/libs/storage_master.h
@@ -4,14 +4,25 @@
 
 ////////
 // GPIOs
-#define EDISON_PWRBTN  PA05
-#define LINUX_ENABLE_POWER PA06
-#define STORAGE_LED    PA07
-
 // Enum indicies must match array in <tock>/kernel/boards/storage_master/src/main.rs
-enum GPIO_Pin_enum{
+#define EDISON_PWRBTN PA05
+#define LINUX_ENABLE_POWER PA06
+#define SD_ENABLE PA21
+enum GPIO_Pin_enum {
     PA05=0,
     PA06,
+    PA21,
+};
+
+////////
+// LEDs
+// Enum indicies must match array in <tock>/kernel/boards/storage_master/src/main.rs
+#define DEBUG_LED0 PB04
+#define DEBUG_LED1 PB05
+#define STORAGE_LED PA07
+enum LED_Pin_enum {
+    PB04=0,
+    PB05,
     PA07,
 };
 
