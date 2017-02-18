47fbc713ce5bc67409d686d282566b4e43fc0de9
Josh Adkins <adkinsjd@umich.edu>
2017-02-18 03:13:35 -0800

    updated the http app for responses

--- software/apps/http_test/main.c
+++ software/apps/http_test/main.c
@@ -20,13 +20,11 @@ int main (void) {
   // Signpost Module Operations
   //
   // Initializations for the rest of the signpost
-  signpost_initialization_module_init(0x50, NULL);
+  signpost_initialization_module_init(0x28, NULL);
   putstr("Initialized\n");
 
-  uint8_t test[600];
+  uint8_t test[200];
 
-  while(1) {
-      delay_ms(1000);
       const char* url = "posttestserver.com/post.php";
       http_request r;
       //the library will automatically throw in content-length
@@ -36,15 +34,37 @@ int main (void) {
       h.header = "content-type";
       h.value = "application/json";
       r.headers = &h;
-      r.body_len = 600;
+      r.body_len = 20;
       r.body = test;
 
-      putstr("About to send\n");
-      http_response result = signpost_networking_post(url, r);
-      putstr("Returned\n");
-      delay_ms(5000);
-  }
+      //prep a place to put responses
+      http_response r2;
+      char reason[50];
+      r2.num_headers = 1;
+      char h1[20];
+      char v1[20];
+      http_header hr;
+      hr.header = h1;
+      hr.header_len = 20;
+      hr.value = v1;
+      hr.value_len = 20;
+      r2.headers = &h;
+      r2.reason = reason;
+      r2.reason_len = 50;
+      r2.body_len = 200;
+      r2.body = test;
 
+    while(1) {
+        delay_ms(1000);
+        putstr("About to send\n");
+        int result = signpost_networking_post(url, r, &r2);
+        if(r2.status == 200) {
+            delay_ms(5000);
+        } else {
+            delay_ms(20000);
+        }
+        putstr("Returned\n");
+    }
 
     //should probably actually setup a watchdog at some point
   ////////////////////////////////////////////////
