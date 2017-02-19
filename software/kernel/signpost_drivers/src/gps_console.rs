4db39840a807891af8137afc60969199ded9b11d
Pat Pannuto <pat.pannuto@gmail.com>
2017-02-19 18:26:33 -0500

    update gps console to tock console

--- software/kernel/signpost_drivers/src/gps_console.rs
+++ software/kernel/signpost_drivers/src/gps_console.rs
@@ -1,9 +1,8 @@
 use core::cell::Cell;
-use kernel::{AppId, AppSlice, Container, Callback, Shared, Driver};
-use kernel::process::Error;
-use kernel::returncode::ReturnCode;
+use kernel::{AppId, AppSlice, Container, Callback, Shared, Driver, ReturnCode};
 use kernel::common::take_cell::TakeCell;
 use kernel::hil::uart::{self, UARTAdvanced, Client};
+use kernel::process::Error;
 
 pub struct App {
     read_callback: Option<Callback>,
@@ -11,6 +10,7 @@ pub struct App {
     read_buffer: Option<AppSlice<Shared, u8>>,
     write_buffer: Option<AppSlice<Shared, u8>>,
     write_len: usize,
+    write_remaining: usize, // How many bytes didn't fit in the buffer and still need to be printed.
     pending_write: bool,
     read_idx: usize,
 }
@@ -23,6 +23,7 @@ impl Default for App {
             read_buffer: None,
             write_buffer: None,
             write_len: 0,
+            write_remaining: 0,
             pending_write: false,
             read_idx: 0,
         }
@@ -66,6 +67,70 @@ impl<'a, U: UARTAdvanced> Console<'a, U> {
             hw_flow_control: false,
         });
     }
+
+    /// Internal helper function for setting up a new send transaction
+    fn send_new(&self, app_id: AppId, app: &mut App, callback: Callback) -> ReturnCode {
+        match app.write_buffer.take() {
+            Some(slice) => {
+                app.write_len = slice.len();
+                app.write_remaining = app.write_len;
+                app.write_callback = Some(callback);
+                self.send(app_id, app, slice);
+                ReturnCode::SUCCESS
+            }
+            None => ReturnCode::EBUSY,
+        }
+    }
+
+    /// Internal helper function for continuing a previously set up transaction
+    /// Returns true if this send is still active, or false if it has completed
+    fn send_continue(&self, app_id: AppId, app: &mut App) -> Result<bool, ReturnCode> {
+        if app.write_remaining > 0 {
+            match app.write_buffer.take() {
+                Some(slice) => {
+                    self.send(app_id, app, slice);
+                    Ok(true)
+                }
+                None => Err(ReturnCode::FAIL),
+            }
+        } else {
+            Ok(false)
+        }
+    }
+
+    /// Internal helper function for sending data for an existing transaction.
+    /// Cannot fail. If can't send now, it will schedule for sending later.
+    fn send(&self, app_id: AppId, app: &mut App, slice: AppSlice<Shared, u8>) {
+        if self.in_progress.get().is_none() {
+            self.in_progress.set(Some(app_id));
+            self.tx_buffer.take().map(|buffer| {
+                let mut transaction_len = app.write_remaining;
+                for (i, c) in slice.as_ref()[slice.len() - app.write_remaining..slice.len()]
+                    .iter()
+                    .enumerate() {
+                    if buffer.len() <= i {
+                        break;
+                    }
+                    buffer[i] = *c;
+                }
+
+                // Check if everything we wanted to print
+                // fit in the buffer.
+                if app.write_remaining > buffer.len() {
+                    transaction_len = buffer.len();
+                    app.write_remaining -= buffer.len();
+                    app.write_buffer = Some(slice);
+                } else {
+                    app.write_remaining = 0;
+                }
+
+                self.uart.transmit(buffer, transaction_len);
+            });
+        } else {
+            app.pending_write = true;
+            app.write_buffer = Some(slice);
+        }
+    }
 }
 
 impl<'a, U: UARTAdvanced> Driver for Console<'a, U> {
@@ -79,12 +144,10 @@ impl<'a, U: UARTAdvanced> Driver for Console<'a, U> {
                         app.read_idx = 0;
                         ReturnCode::SUCCESS
                     })
-                    .unwrap_or_else(|err| {
-                        match err {
-                            Error::OutOfMemory => ReturnCode::ENOMEM,
-                            Error::AddressOutOfBounds => ReturnCode::EINVAL,
-                            Error::NoSuchApp => ReturnCode::EINVAL,
-                        }
+                    .unwrap_or_else(|err| match err {
+                        Error::OutOfMemory => ReturnCode::ENOMEM,
+                        Error::AddressOutOfBounds => ReturnCode::EINVAL,
+                        Error::NoSuchApp => ReturnCode::EINVAL,
                     })
             }
             // Allow a write buffer
@@ -94,12 +157,10 @@ impl<'a, U: UARTAdvanced> Driver for Console<'a, U> {
                         app.write_buffer = Some(slice);
                         ReturnCode::SUCCESS
                     })
-                    .unwrap_or_else(|err| {
-                        match err {
-                            Error::OutOfMemory => ReturnCode::ENOMEM,
-                            Error::AddressOutOfBounds => ReturnCode::EINVAL,
-                            Error::NoSuchApp => ReturnCode::EINVAL,
-                        }
+                    .unwrap_or_else(|err| match err {
+                        Error::OutOfMemory => ReturnCode::ENOMEM,
+                        Error::AddressOutOfBounds => ReturnCode::EINVAL,
+                        Error::NoSuchApp => ReturnCode::EINVAL,
                     })
             }
             _ => ReturnCode::ENOSUPPORT,
@@ -109,33 +170,12 @@ impl<'a, U: UARTAdvanced> Driver for Console<'a, U> {
     fn subscribe(&self, subscribe_num: usize, callback: Callback) -> ReturnCode {
         match subscribe_num {
             0 /* read line */ => {
-                panic!("readline is borked right now. Don't call it");
+                // read line is not implemented for console at this time
+                ReturnCode::ENOSUPPORT
             },
             1 /* putstr/write_done */ => {
                 self.apps.enter(callback.app_id(), |app, _| {
-                    match app.write_buffer.take() {
-                        Some(slice) => {
-                            app.write_callback = Some(callback);
-                            app.write_len = slice.len();
-                            if self.in_progress.get().is_none() {
-                                self.in_progress.set(Some(callback.app_id()));
-                                self.tx_buffer.take().map(|buffer| {
-                                    for (i, c) in slice.as_ref().iter().enumerate() {
-                                        if buffer.len() <= i {
-                                            break;
-                                        }
-                                        buffer[i] = *c;
-                                    }
-                                    self.uart.transmit(buffer, app.write_len);
-                                });
-                            } else {
-                                app.pending_write = true;
-                                app.write_buffer = Some(slice);
-                            }
-                            ReturnCode::SUCCESS
-                        },
-                        None => ReturnCode::FAIL
-                    }
+                    self.send_new(callback.app_id(), app, callback)
                 }).unwrap_or_else(|err| {
                     match err {
                         Error::OutOfMemory => ReturnCode::ENOMEM,
@@ -172,65 +212,80 @@ impl<'a, U: UARTAdvanced> Driver for Console<'a, U> {
                     }
                 })
             },
-            _ => ReturnCode::ENOSUPPORT,
+            _ => ReturnCode::ENOSUPPORT
         }
     }
 
     fn command(&self, cmd_num: usize, arg1: usize, _: AppId) -> ReturnCode {
         match cmd_num {
-            0 /* putc */ => {
+            0 /* check if present */ => ReturnCode::SUCCESS,
+            1 /* putc */ => {
                 self.tx_buffer.take().map(|buffer| {
                     buffer[0] = arg1 as u8;
                     self.uart.transmit(buffer, 1);
                 });
-                ReturnCode::SuccessWithValue{ value: 1 }
+                ReturnCode::SuccessWithValue { value: 1 }
             },
-            _ => ReturnCode::ENOSUPPORT,
+            _ => ReturnCode::ENOSUPPORT
         }
     }
 }
 
 impl<'a, U: UARTAdvanced> Client for Console<'a, U> {
     fn transmit_complete(&self, buffer: &'static mut [u8], _error: uart::Error) {
-        // Write TX is done, notify appropriate app and start another
-        // transaction if pending
+        // Either print more from the AppSlice or send a callback to the
+        // application.
         self.tx_buffer.replace(buffer);
         self.in_progress.get().map(|appid| {
             self.in_progress.set(None);
             self.apps.enter(appid, |app, _| {
-                app.write_callback.map(|mut cb| {
-                    cb.schedule(app.write_len, 0, 0);
-                });
-                app.write_len = 0;
+                match self.send_continue(appid, app) {
+                    Ok(more_to_send) => {
+                        if !more_to_send {
+                            // Go ahead and signal the application
+                            let written = app.write_len;
+                            app.write_len = 0;
+                            app.write_callback.map(|mut cb| { cb.schedule(written, 0, 0); });
+                        }
+                    }
+                    Err(return_code) => {
+                        // XXX This shouldn't ever happen?
+                        app.write_len = 0;
+                        app.write_remaining = 0;
+                        app.pending_write = false;
+                        let r0 = isize::from(return_code) as usize;
+                        app.write_callback.map(|mut cb| { cb.schedule(r0, 0, 0); });
+                    }
+                }
             })
         });
 
-        for cntr in self.apps.iter() {
-            let started_tx = cntr.enter(|app, _| {
-                if app.pending_write {
-                    app.pending_write = false;
-                    app.write_buffer
-                        .as_ref()
-                        .map(|slice| {
-                            self.tx_buffer.take().map(|buffer| {
-                                for (i, c) in slice.as_ref().iter().enumerate() {
-                                    if buffer.len() <= i {
-                                        break;
-                                    }
-                                    buffer[i] = *c;
-                                }
-                                self.uart.transmit(buffer, app.write_len);
-                            });
-                            self.in_progress.set(Some(app.appid()));
-                            true
-                        })
-                        .unwrap_or(false)
-                } else {
-                    false
+        // If we are not printing more from the current AppSlice,
+        // see if any other applications have pending messages.
+        if self.in_progress.get().is_none() {
+            for cntr in self.apps.iter() {
+                let started_tx = cntr.enter(|app, _| {
+                    if app.pending_write {
+                        app.pending_write = false;
+                        match self.send_continue(app.appid(), app) {
+                            Ok(more_to_send) => more_to_send,
+                            Err(return_code) => {
+                                // XXX This shouldn't ever happen?
+                                app.write_len = 0;
+                                app.write_remaining = 0;
+                                app.pending_write = false;
+                                let r0 = isize::from(return_code) as usize;
+                                app.write_callback.map(|mut cb| { cb.schedule(r0, 0, 0); });
+                                false
+                            }
+                        }
+                    } else {
+                        false
+                    }
+                });
+                if started_tx {
+                    break;
                 }
-            });
-            if started_tx {
-                break;
             }
         }
     }
