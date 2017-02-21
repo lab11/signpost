4ae2219a6f6c52a197dd8b619735c96cd86f3c37
Neal Jackson <nealjack@umich.edu>
2017-02-21 16:18:55 -0600

    add template app
    starting point for tutorials/new apps

---
+++ software/apps/template/main.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <inttypes.h>
+
+#include "tock.h"
+#include "console.h"
+#include "timer.h"
+#include "signpost_api.h"
+#include "signbus_io_interface.h"
+
+#define I2C_ADDRESS 0x15
+
+int main(void) {
+    // initialize app with
+    signpost_initialization_module_init(I2C_ADDRESS, SIGNPOST_INITIALIZATION_NO_APIS);
+
+    /* YOUR CODE GOES HERE */
+
+}
