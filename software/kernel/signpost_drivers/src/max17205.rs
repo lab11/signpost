825cb1696f4ce763fe28955a4ff38c62afe98e59
Neal Jackson <nealjack@umich.edu>
2017-03-24 14:29:06 -0700

    max17205 driver compiles
    Added to controller main.rs
    
    Added README with all allocated signpost drivers

--- software/kernel/signpost_drivers/src/max17205.rs
+++ software/kernel/signpost_drivers/src/max17205.rs
@@ -1,6 +1,5 @@
 use kernel::common::take_cell::TakeCell;
 use core::cell::Cell;
-use kernel::hil::gpio;
 use kernel::hil::i2c;
 use kernel::{AppId, Callback, Driver};
 use kernel::returncode::ReturnCode;
@@ -10,6 +9,7 @@ pub static mut BUFFER: [u8; 8] = [0; 8];
 // Addresses 0x000 - 0x0FF, 0x180 - 0x1FF can be written as blocks
 // Addresses 0x100 - 0x17F must be written by word
 #[allow(dead_code)]
+#[allow(non_camel_case_types)]
 enum Registers {
     Status      = 0x000,
     RepCap      = 0x005, // Reported capacity, LSB = 0.5 mAh
@@ -22,6 +22,7 @@ enum Registers {
 }
 
 #[derive(Clone,Copy,PartialEq)]
+#[allow(dead_code)]
 enum State {
     Idle,
 
@@ -40,53 +41,57 @@ enum State {
 }
 
 pub trait MAX17205Client {
-    fn interrupt(&self);
-    fn status();
+    //fn status(&self);
     fn state_of_charge(&self, percent: u16, capacity: u16, full_capacity: u16);
     fn voltage_current(&self, voltage: u16, current: u16);
-    fn done(&self)
+    fn done(&self);
 }
 
 pub struct MAX17205<'a> {
-    i2c0: &'a i2c:I2CDevice,
-    i2c1: &'a i2c:I2CDevice,
+    i2c0: &'a i2c::I2CDevice,
+    i2c1: &'a i2c::I2CDevice,
     state: Cell<State>,
-    status: Cell<u8>,
+    //status: Cell<u8>,
     soc: Cell<u16>,
     soc_mah: Cell<u16>,
-    full_mah: Cell<u16>,
+    //full_mah: Cell<u16>,
     voltage: Cell<u16>,
-    current: Cell<u16>,
+    //current: Cell<u16>,
     buffer: TakeCell<'static, [u8]>,
-    client: Cell<Option<&'static LTC2941Client>>,
+    client: Cell<Option<&'static MAX17205Client>>,
 }
 
 impl<'a> MAX17205<'a> {
     pub fn new(i2c0: &'a i2c::I2CDevice,
                i2c1: &'a i2c::I2CDevice,
                buffer: &'static mut [u8])
-               -> LTC2941<'a> {
+               -> MAX17205<'a> {
         // setup and return struct
-        LTC2941 {
+        MAX17205 {
             i2c0: i2c0,
             i2c1: i2c1,
             state: Cell::new(State::Idle),
+            soc: Cell::new(0),
+            soc_mah: Cell::new(0),
+            voltage: Cell::new(0),
             buffer: TakeCell::new(buffer),
             client: Cell::new(None),
         }
     }
 
-    pub fn set_client<C: LTC2941Client>(&self, client: &'static C) {
+    pub fn set_client<C: MAX17205Client>(&self, client: &'static C) {
         self.client.set(Some(client));
     }
 
     fn configure(&self) {
         self.buffer.take().map(|buffer| {
-            let packcfg = 0x0A03 //See PackCfg (59) and typical circuit (28)
+            self.i2c1.enable();
+
+            let packcfg = 0x0A03; //See PackCfg (59) and typical circuit (28)
             // Memory address (always lower byte, but I2C address is different)
-            buffer[0] = (Registers::nPackCfg & 0xFF) as u8
-            buffer[1] = (packcfg & 0xFF) as u8
-            buffer[2] = ((packcfg >> 8) & 0xFF) as u8
+            buffer[0] = ((Registers::nPackCfg as u8) & 0xFF) as u8;
+            buffer[1] = (packcfg & 0xFF) as u8;
+            buffer[2] = ((packcfg >> 8) & 0xFF) as u8;
 
             self.i2c1.write(buffer, 3);
             self.state.set(State::Done);
@@ -104,26 +109,26 @@ impl<'a> MAX17205<'a> {
 
     fn setup_read_soc(&self) {
         self.buffer.take().map(|buffer| {
-            self.i2c.enable();
+            self.i2c0.enable();
 
             // Get SOC mAh and percentage
             // Write reqcap address
-            buffer[0] = (Registers::RepSOC & 0xFF) as u8
+            buffer[0] = ((Registers::RepSOC as u8) & 0xFF) as u8;
             self.i2c0.write(buffer, 1);
-            self.state.set(Registers::SetupReadSOC);
-        })
+            self.state.set(State::SetupReadSOC);
+        });
     }
 
     fn setup_read_curvolt(&self) {
         self.buffer.take().map(|buffer| {
-            self.i2c.enable();
+            self.i2c0.enable();
 
             // Get current and voltage
             // Write Batt address
-            buffer[0] = (Registers::Batt & 0xFF) as u8
+            buffer[0] = ((Registers::Batt as u8) & 0xFF) as u8;
             self.i2c0.write(buffer, 1);
             self.state.set(State::SetupReadVolt);
-        })
+        });
     }
 }
 
@@ -133,12 +138,12 @@ impl<'a> i2c::I2CClient for MAX17205<'a> {
             State::SetupReadSOC => {
                 // Write of SOC memory address complete, now issue read
                 self.i2c0.read(buffer, 4);
-                self.state.set(Registers::ReadSOC);
+                self.state.set(State::ReadSOC);
             },
             State::ReadSOC => {
                 // Read of SOC memory address complete
-                self.soc_mah.set((buffer[1] as u16 << 8) | (buffer[0] as u16));
-                self.soc.set((buffer[3] as u16 << 8) | (buffer[2] as u16));
+                self.soc_mah.set(((buffer[1] as u16) << 8) | (buffer[0] as u16));
+                self.soc.set(((buffer[3] as u16) << 8) | (buffer[2] as u16));
 
                 self.buffer.replace(buffer);
 
@@ -147,62 +152,62 @@ impl<'a> i2c::I2CClient for MAX17205<'a> {
                 self.buffer.take().map(|selfbuf| {
                     // Get SOC mAh and percentage
                     // Write reqcap address
-                    selfbuf[0] = (Registers::FullCapRep & 0xFF) as u8
+                    selfbuf[0] = ((Registers::FullCapRep as u8) & 0xFF) as u8;
                     self.i2c0.write(selfbuf, 1);
 
-                    self.state.set(Registers::SetupReadCap);
-                })
+                    self.state.set(State::SetupReadCap);
+                });
             },
             State::SetupReadCap => {
                 // Now issue read
                 self.i2c0.read(buffer, 2);
-                self.state.set(Registers::ReadCap);
+                self.state.set(State::ReadCap);
             },
             State::ReadCap => {
-                let full_mah = (buffer[1] as u16 << 8) | (buffer[0] as u16);
+                let full_mah = ((buffer[1] as u16) << 8) | (buffer[0] as u16);
 
                 self.client.get().map(|client| {
-                    client.state_of_charge(self.soc, self.soc_mah, full_mah);
+                    client.state_of_charge(self.soc.get(), self.soc_mah.get(), full_mah);
                 });
 
                 self.buffer.replace(buffer);
-                self.i2c.disable();
+                self.i2c0.disable();
                 self.state.set(State::Idle);
             },
             State::SetupReadVolt => {
                 // Write of voltage memory address complete, now issue read
                 self.i2c0.read(buffer, 2);
-                self.state.set(Registers::ReadVolt);
+                self.state.set(State::ReadVolt);
             },
             State::ReadVolt => {
                 // Read of voltage memory address complete
-                self.voltage.set((buffer[1] as u16 << 8) | (buffer[0] as u16));
+                self.voltage.set(((buffer[1] as u16) << 8) | (buffer[0] as u16));
 
                 self.buffer.replace(buffer);
 
                 // Now issue write of memory address of current
                 // Setup read capacity
                 self.buffer.take().map(|selfbuf| {
-                    buffer[0] = (Registers::Current & 0xFF) as u8
-                    self.i2c0.write(buffer, 1);
+                    selfbuf[0] = ((Registers::Current as u8) & 0xFF) as u8;
+                    self.i2c0.write(selfbuf, 1);
 
-                    self.state.set(Registers::SetupReadCurrent);
-                })
+                    self.state.set(State::SetupReadCurrent);
+                });
             },
             State::SetupReadCurrent => {
                 // Now issue read
                 self.i2c0.read(buffer, 2);
-                self.state.set(Registers::ReadCurrent);
+                self.state.set(State::ReadCurrent);
             },
-            State::ReadCap => {
-                let current = (buffer[1] as u16 << 8) | (buffer[0] as u16);
+            State::ReadCurrent => {
+                let current = ((buffer[1] as u16) << 8) | (buffer[0] as u16);
 
                 self.client.get().map(|client| {
-                    client.voltage_current(self.voltage, self.current);
+                    client.voltage_current(self.voltage.get(), current);
                 });
 
                 self.buffer.replace(buffer);
-                self.i2c.disable();
+                self.i2c0.disable();
                 self.state.set(State::Idle);
             },
             State::Done => {
@@ -211,10 +216,87 @@ impl<'a> i2c::I2CClient for MAX17205<'a> {
                 });
 
                 self.buffer.replace(buffer);
-                self.i2c.disable();
+                self.i2c0.disable();
+                self.i2c1.disable();
                 self.state.set(State::Idle);
             },
             _ => {}
+        }
+    }
+}
+
+/// Default implementation of the MAX17205 driver that provides a Driver
+/// interface for providing access to applications.
+pub struct MAX17205Driver<'a> {
+    max17205: &'a MAX17205<'a>,
+    callback: Cell<Option<Callback>>,
+}
+
+impl<'a> MAX17205Driver<'a> {
+    pub fn new(max: &'a MAX17205) -> MAX17205Driver<'a> {
+        MAX17205Driver {
+            max17205: max,
+            callback: Cell::new(None),
+        }
+    }
+}
+
+impl<'a> MAX17205Client for MAX17205Driver<'a> {
+    fn state_of_charge(&self, percent: u16, capacity: u16, full_capacity: u16) {
+        self.callback.get().map(|mut cb| {
+            cb.schedule(1, percent as usize, (capacity as usize) << 16 | (full_capacity as usize));
+        });
+    }
+
+    fn voltage_current(&self, voltage: u16, current: u16) {
+        self.callback.get().map(|mut cb| {
+            cb.schedule(2, voltage as usize, current as usize)
+        });
+    }
+
+    fn done(&self) {
+        self.callback.get().map(|mut cb| {
+            cb.schedule(3, 0, 0);
+        });
+    }
+}
+
+impl<'a> Driver for MAX17205Driver<'a> {
+    fn subscribe(&self, subscribe_num: usize, callback: Callback) -> ReturnCode {
+        match subscribe_num {
+            0 => {
+                self.callback.set(Some(callback));
+                ReturnCode::SUCCESS
+            }
+
+            // default
+            _ => ReturnCode::ENOSUPPORT,
+        }
+    }
+
+#[allow(unused_variables)]
+    fn command(&self, command_num: usize, data: usize, _: AppId) -> ReturnCode {
+        match command_num {
+            // configure
+            0 => {
+                self.max17205.configure();
+                ReturnCode::SUCCESS
+            }
+
+            // get soc
+            1 => {
+                self.max17205.setup_read_soc();
+                ReturnCode::SUCCESS
+            }
+
+            // get voltage & current
+            2 => {
+                self.max17205.setup_read_curvolt();
+                ReturnCode::SUCCESS
+            }
+
+            // default
+            _ => ReturnCode::ENOSUPPORT,
         }
     }
 }
