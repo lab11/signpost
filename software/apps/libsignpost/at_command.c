7ed540e91cc659a3d99e593095b9bc2f765ee0f8
Josh Adkins <adkinsjd@umich.edu>
2017-05-26 19:16:04 -0700

    added custom checking functions to the wait interface

--- software/apps/libsignpost/at_command.c
+++ software/apps/libsignpost/at_command.c
@@ -22,6 +22,18 @@ static int check_buffer(uint8_t* buf, int len) {
     return AT_NO_RESPONSE;
 }
 
+static int check_custom_buffer(uint8_t* buf, int len, const char* rstring) {
+
+    int rlen = strlen(rstring);
+    if(len >= rlen) {
+        if(!strncmp(buf+len-rlen,rstring,rlen)) {
+            return AT_SUCCESS;
+        }
+    }
+
+    return AT_NO_RESPONSE;
+}
+
 int at_send(int console_num, const char* cmd) {
     console_write(console_num, (uint8_t*) cmd, strlen(cmd));
     return AT_SUCCESS;
@@ -38,14 +50,39 @@ int at_wait_for_response(int console_num, uint8_t max_tries) {
     return at_get_response(console_num, max_tries, buf, 200);
 }
 
+int at_wait_for_custom_response(int console_num, uint8_t max_tries, const char* rstring) {
+    static uint8_t buf[200];
+    return at_get_custom_response(console_num, max_tries, buf, 200, rstring);
+}
+
 int at_get_response(int console_num, uint8_t max_tries, uint8_t* buf, size_t max_len) {
 
     int tlen = 0;
     for(uint8_t i = 0; i < max_tries; i++) {
 
-        int len = console_read(console_num, buf+tlen, max_len);
+        int len = console_read(console_num, buf+tlen, max_len-tlen);
         tlen += len;
         int check = check_buffer(buf, tlen);
+
+        if(check == AT_SUCCESS) {
+            return tlen;
+        } else if (check == AT_ERROR) {
+            return AT_ERROR;
+        }
+    }
+
+    return AT_NO_RESPONSE;
+}
+
+int at_get_custom_response(int console_num, uint8_t max_tries, uint8_t* buf, size_t max_len, const char* rstring) {
+
+    int tlen = 0;
+    for(uint8_t i = 0; i < max_tries; i++) {
+
+        int len = console_read(console_num, buf+tlen, max_len-tlen);
+        printf("Got len: %d\n", len);
+        tlen += len;
+        int check = check_custom_buffer(buf, tlen, rstring);
 
         if(check == AT_SUCCESS) {
             return tlen;
--- software/apps/libsignpost/at_command.h
+++ software/apps/libsignpost/at_command.h
@@ -10,4 +10,6 @@ int at_send(int console_num, const char* cmd);
 int at_send_buf(int console_num, uint8_t* buf, size_t len);
 
 int at_wait_for_response(int console_num, uint8_t max_tries);
+int at_wait_for_custom_response(int console_num, uint8_t max_tries, const char* rstring);
 int at_get_response(int console_num, uint8_t max_tries, uint8_t* buf, size_t max_len);
+int at_get_custom_response(int console_num, uint8_t max_tries, uint8_t* buf, size_t max_len, const char* rstring);
