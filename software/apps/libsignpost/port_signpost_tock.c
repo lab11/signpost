22e2e8ab25df0bbaf9389970ccd7baca7e90eed5
Joshua Adkuins <adkins@berkeley.edu>
2017-05-27 14:35:10 -0700

    made a file for the tock port implementation

---
+++ software/apps/libsignpost/port_signpost_tock.c
@@ -0,0 +1,60 @@
+#include <stdint.h>
+#include "tock.h"
+#include "port_signpost.h"
+#include "i2c_master_slave.h"
+
+/* This is a port interface for the signpost API and networking stack!
+The goal is to export the minimal interface such that platforms
+can integrate with the signpost.
+
+From a high level you will need to provide:
+    - Blocking I2C write function (currently at least 255 bytes at a time)
+    - Asynchronous I2C slave function (listen on an address and send 
+                                        back written bytes asynchronously)
+    - Set and clear of the MOD_OUT pin
+    - Asynchronous callback for falling edge of the Mod-in pin
+    - Currently the signpost libraries assume printf functionality
+        - This is assumed to be provided through <stdio.h>
+
+To provide this functionality please implement the following interface
+for each platform*/
+
+
+static uint8_t port_i2c_address;
+
+//This function is called upon signpost initialization
+//You should use it to set up the i2c interface
+void port_signpost_init(uint8_t i2c_address) {
+    port_i2c_address = i2c_address; 
+}
+
+//This function is a blocking i2c send call
+//it should return the length of the message successfully sent on the bus
+//If the bus returns an error, use the appropriate error code
+//defined in this file
+int port_signpost_i2c_master_write(uint8_t* buf, int len) {
+
+}
+
+//This function is sets up the asynchronous i2c receive interface
+//When this function is called start listening on the i2c bus for
+//The address specified in init
+//Place data in the buffer no longer than the max len
+void port_signpost_i2c_slave_listen(port_signpost_i2c_slave_write_callback cb, uint8_t* buf, int max_len) {
+
+}
+
+//These functions are used to control the mod_out pin
+void port_signpost_mod_out_set(void) {
+
+}
+
+void port_signpost_mod_out_clear(void) {
+
+}
+
+//This function is used to get the input interrupt for the falling edge of
+//mod-in
+void port_signpost_mod_in_falling_edge_listen(port_signpost_mod_in_falling_edge_callback cb) {
+
+}
