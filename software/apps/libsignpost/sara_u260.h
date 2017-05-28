5ec1e4b2d1edd5944ed5b8a153a567e1c10faf83
Joshua Adkuins <adkins@berkeley.edu>
2017-05-28 16:32:48 -0700

    a comment in the u260 header

--- software/apps/libsignpost/sara_u260.h
+++ software/apps/libsignpost/sara_u260.h
@@ -1,3 +1,6 @@
+//A userland driver for the UBLOX SARA-U260
+//https://www.u-blox.com/en/product/sara-u2-series
+
 #include <stdint.h>
 #include <tock.h>
 
@@ -6,8 +9,15 @@
 #define SARA_U260_INVALIDPARAM -2
 #define SARA_U260_NO_SERVICE -3
 
+//initializes and turns off command echo
 int sara_u260_init(void);
+
+//Attempts to perform an HTTP Post
+//Will attempt to join the network and setup a packet switch connect if one does not exist
 int sara_u260_basic_http_post(const char* url, const char* path, uint8_t* buf, size_t len);
 
+//Returns the response from the most recent successful post
 int sara_u260_get_post_response(uint8_t* buf, size_t max_len);
+
+//Returns part of the response from the most recent successful post
 int sara_u260_get_post_partial_response(uint8_t* buf, size_t offset, size_t max_len);
